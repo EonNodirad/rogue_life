a27a1f2e9f69d22eb9c9f61a8f7bcb946a66a972
Noé Daridon <noe.daridon@gmail.com>
2026-04-06 11:11:53 +0200

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,32 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Rogue Life',
+        short_name: 'RogueLife',
+        description: 'Un super dungeon crawler',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 });
